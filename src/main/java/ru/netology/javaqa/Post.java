57b508ac31001cced2412631d331841b834dad32
Your NameDaniya <you@example.com>
2023-10-31 00:22:34 +0300

    Initial commit

---
+++ src/main/java/ru/netology/javaqa/Post.java
@@ -0,0 +1,11 @@
+package ru.netology.javaqa;
+
+public class Post {
+    public String name;
+    public String passportNumber;
+    public String patronymic;
+    public String phoneNumber;
+    public String surname;
+    public boolean subscription;
+    public FormDate birthday;
+}
